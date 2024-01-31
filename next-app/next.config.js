f89132aeddd2325f6b21f2c2f67201646b2cca4a
Quang Trịnh Văn <quang.trinhvan1@vti.com.vn>
2024-01-31 18:32:33 +0700

    update config

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -7,6 +7,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
+    // basePath: '/nextapp',
+    // assetPrefix: process.env.BASE_PATH || '',
+    // publicRuntimeConfig: {
+    //     basePath: process.env.BASE_PATH || '',
+    // },
+    // distDir: 'out',
 };
   
 module.exports = nextConfig;

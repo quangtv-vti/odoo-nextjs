9b849e11359eba38ce2d44183838349d4c7aad9c
quangtv <trinhvanquangf1@gmail.com>
2024-02-06 07:26:58 +0700

    add taiwind css & set netxt at 14.0.4

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -1,18 +1,4 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "standalone",
-    // basePath: '/nextapp',
-    // assetPrefix: process.env.BASE_PATH || '',
-    // publicRuntimeConfig: {
-    //     basePath: process.env.BASE_PATH || '',
-    // },
-    // distDir: 'out',
-};
-  
-module.exports = nextConfig;
+const nextConfig = {}
+
+module.exports = nextConfig
